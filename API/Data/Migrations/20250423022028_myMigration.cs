4a9df6438a1a4091469e0463613d50c3d9473332
demian824 <demian824@gmail.com>
2025-05-07 14:31:46 -0400

    End of section 10

---
+++ API/Data/Migrations/20250423022028_myMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class myMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descriptrion",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "Descriptrion");
+        }
+    }
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,24 @@
+using System;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
+{
+    public required DbSet<Product> Products { get; set; }
+    public required DbSet<Basket> Baskets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<IdentityRole>()
+               .HasData(
+                    new IdentityRole { Id = "984162d1-e8e8-4e77-ac29-11f13b3f93cf", Name = "Member", NormalizedName ="MEMBER" },
+                    new IdentityRole { Id = "59320e91-c661-4848-979c-32abde96faa1",  Name = "Admin", NormalizedName = "ADMIN" }
+               );
+    }
+    
+}
